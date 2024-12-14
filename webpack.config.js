8abe791d4de1eb025743ff9c8b8c057306891d5c
Domusgpt <Phillips.paul.email@gmail.com>
2024-12-13 19:22:06 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,41 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: "./js/main.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html"
-        })
+  entry: './js/main.js', // Entry point for your app
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Output JavaScript file
+  },
+  mode: 'production', // Use 'development' for local testing
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // Match all CSS files
+        use: ['style-loader', 'css-loader'], // Process and inject CSS
+      },
+      {
+        test: /\.js$/, // Match all JavaScript files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Transpile JavaScript
+          options: {
+            presets: ['@babel/preset-env'], // Modern JavaScript compatibility
+          },
+        },
+      },
     ],
-    devServer: {
-        static: "./dist",
-        hot: true,
-        open: true,
-        port: 3001
-    },
-    resolve: {
-        extensions: [".js"]
-    }
-}
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Cleans the output directory before each build
+    new HtmlWebpackPlugin({
+      template: './index.html', // Use your `index.html` as a template
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'), // Serve files from `dist/`
+    port: 3000, // Local server runs on port 3000
+    hot: true, // Enable Hot Module Replacement
+  },
+};
